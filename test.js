27689e83e6a39e3514b50e19aa6a57b06d7970c6
Yaroslav Shulga <yaroslav.shulga@sky-tech.co>
2018-08-26 20:30:02 +0300

    asd

---
+++ test.js
@@ -0,0 +1,13 @@
+module.exports.a = test => console.log(11);
+
+
+/*
+const arr = [];
+
+for (let i = 0; i < 10000; i++) {
+    arr.push(Math.random() * i);
+    if (i === 50000) {
+        console.warn(`now 500000`);
+    }
+    arr.sort();
+}*/
