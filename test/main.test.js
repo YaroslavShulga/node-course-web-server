1a908fe4fe66db84d31e05666ff7d845b60f7cf7
Yaroslav Shulga <yaroslav.shulga@sky-tech.co>
2018-09-18 19:50:35 +0300

    sad

---
+++ test/main.test.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+var expect = require("chai").expect;
+
+describe("Main file test", () => {
+	it("equal", () => {
+		expect(5, "mother fuck").to.be.equal(5);
+		expect("hey").to.be.a("string");
+		expect(true).is.true;
+	});
+
+	it("write this case");
+});
